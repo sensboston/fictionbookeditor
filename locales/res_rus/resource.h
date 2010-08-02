f68fb890a63b051229df58e298cf42f96db8e332
sens.boston <sens.boston@25836250-9a28-bdbc-e09c-ff2305ad1a9e>
2010-08-02 12:31:18 +0000

    - added hotkeys for "Subscript" and "Superscript" commands (issue #129)

--- locales/res_rus/resource.h
+++ locales/res_rus/resource.h
@@ -276,6 +276,8 @@
 #define IDS_UPDATEEXISTS                301
 #define IDS_UPDATE_DOWNLOADREADY        302
 #define IDS_BAD_XML_MSG                 303
+#define IDS_HOTKEY_EDIT_SUB             304
+#define IDS_HOTKEY_EDIT_SUP             305
 #define IDB_UPD_CHECK1                  400
 #define IDB_UPD_CHECK2                  401
 #define IDB_UPD_CHECK3                  402
