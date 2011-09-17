b0578c95ac7320c35134012f7ddcf1a01acc38a6
sclex2 <sclex2@25836250-9a28-bdbc-e09c-ff2305ad1a9e>
2011-09-17 18:47:51 +0000

    - скрипт "Регистр заголовков и подзаголовков" вылетал, если в документе были пустые заголовки или подзаголовки.

--- trunk/files/Scripts/01_Регистр/08_Регистр заголовков и подзаголовков.js
+++ trunk/files/Scripts/01_Регистр/08_Регистр заголовков и подзаголовков.js
@@ -3,7 +3,7 @@
 //сайт скриптов: http://scripts.fictionbook.org
 
 function Run() {
- var elementBrowser_versionNum="1.5";
+ var elementBrowser_versionNum="1.6";
  var dialogWidth="800px";
  var dialogHeight="540px";
  var fbwBody=document.getElementById("fbw_body");
