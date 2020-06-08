4db3d3590401035b233cab669a0903c51ddc9351
User01 <sclex2@gmail.com>
2020-06-08 17:00:57 +0300

    Скрипт "Создать сноски из выделенных абзацев" обновлен до версии 1.1. Изменение: теперь после удаления маркеров знака сноски будут удаляться оставшиеся в документе пустые теги - "<emphasis></emphasis>", "<strong></strong>" и т.п.

--- files/Scripts/02_Примечания/12_Создать сноски из выделенных абзацев....js
+++ files/Scripts/02_Примечания/12_Создать сноски из выделенных абзацев....js
@@ -1,7 +1,7 @@
 //Скрипт «Создать сноски из выделенных абзацев»
 //Автор Sclex
 
-var notesFromGroupedParagraphs_versionNum="1.0";
+var notesFromSelectedParagraphs_versionNum="1.1";
 
 function Run() {
  var dialogWidth="700px";
@@ -11,7 +11,7 @@ function Run() {
  coll["fbwBody"]=fbwBody;
  coll["mainDocument"]=document;
  coll["window"]=window;
- coll["versionNum"]=notesFromGroupedParagraphs_versionNum;
+ coll["versionNum"]=notesFromSelectedParagraphs_versionNum;
  var modes=window.showModelessDialog("HTML/Создать сноски из выделенных абзацев - задание параметров.htm",coll,
      "dialogHeight: "+dialogHeight+"; dialogWidth: "+dialogWidth+"; "+
      "center: Yes; help: No; resizable: Yes; status: No;");
