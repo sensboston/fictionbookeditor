12b045ed11f2d778367055c2a182932d8f71d9c3
User01 <sclex2@gmail.com>
2020-06-24 12:13:22 +0300

    Скрипт "Создать сноски из выделенных абзацев..." обновлен до версии 1.2. Изменение - теперь не находятся якобы-маркеры-знака-сноски, находящиеся внутри тега ссылки.

--- files/Scripts/02_Примечания/12_Создать сноски из выделенных абзацев....js
+++ files/Scripts/02_Примечания/12_Создать сноски из выделенных абзацев....js
@@ -1,7 +1,7 @@
 //Скрипт «Создать сноски из выделенных абзацев»
 //Автор Sclex
 
-var notesFromSelectedParagraphs_versionNum="1.1";
+var notesFromSelectedParagraphs_versionNum="1.2";
 
 function Run() {
  var dialogWidth="700px";
