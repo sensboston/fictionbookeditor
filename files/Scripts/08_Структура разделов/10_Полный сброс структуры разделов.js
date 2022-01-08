924b7b053f463a47e6aa18b9baecf96de2cdb9dd
User01 <sclex2@gmail.com>
2022-01-08 10:33:42 +0300

    - Добавлен новый скрипт "10_Полный сброс структуры разделов" (в папку "08_Структура разделов").

---
+++ files/Scripts/08_Структура разделов/10_Полный сброс структуры разделов.js
@@ -0,0 +1,65 @@
+//version 1.0
+
+function Run() {
+
+ var newSect, htmlStr, firstSection, wasClosingSection;
+
+ function rec(el) {
+  var saveNext,saveNext2,el2;
+  while (el) {
+   saveNext=el.nextSibling;
+   if (el.nodeName=="DIV" && el.className!="section") {
+     if (el.className!="title" && el.className!="epigraph" && el.className!="annotation"
+         || (el.className=="title" && el.parentNode.className=="body"))
+       htmlStr+="<DIV class="+el.className+(el.getAttribute("id")?" id='"+el.getAttribute("id")+"'":"")+">";
+     rec(el.firstChild);
+     if (el.className!="title" && el.className!="epigraph" && el.className!="annotation"
+         || (el.className=="title" && el.parentNode.className=="body"))
+       htmlStr+="</DIV>";
+   }
+   if (el.nodeName=="DIV" && el.className=="body" && el.firstChild) {
+     if (el.getAttribute("fbname")!="notes" && el.getAttribute("fbname")!="comments") {
+       htmlStr="<DIV class=body"+(el.getAttribute("id")?" id='"+el.getAttribute("id")+"'":"")+">";
+       firstSection=true;
+       wasClosingSection=false;
+       rec(el.firstChild);
+       if (wasClosingSection) htmlStr+="</DIV>";
+       htmlStr+="</DIV>";
+       //alert(htmlStr);
+       el.innerHTML="<P>1</P>";
+       newSect=el.insertAdjacentHTML("beforeBegin",htmlStr);
+       InflateIt(newSect);
+       el.innerHTML="";
+       el.removeNode(true);
+     }
+   }
+   if (el.nodeName=="DIV" && el.className=="section") {
+     if (firstSection) {
+       htmlStr+="<DIV class=section"+(el.getAttribute("id")?" id='"+el.getAttribute("id")+"'":"")+">";
+       firstSection=false;
+       wasClosingSection=true;
+     }
+     rec(el.firstChild);
+   }
+   if (el.nodeName=="P") {
+     if (el.className=="text-author" && el.parentNode.className=="epigraph")
+       htmlStr+="<P"+(el.getAttribute("id")?" id='"+el.getAttribute("id")+"'":"")+">"+el.innerHTML+"</P>";
+     else  
+       htmlStr+=el.outerHTML;
+   }
+   el=saveNext;
+  }
+ }
+
+ window.external.BeginUndoUnit(document,"full reset of structure of sections");
+ var fbwBody=document.getElementById("fbw_body");
+ if (!fbwBody) return;
+ if (fbwBody.firstChild)
+   rec(fbwBody.firstChild);
+ window.external.EndUndoUnit(document);
+ try {
+   window.external.SetStatusBarText("Full reset of structure of sections is successfully finished.");
+ }
+ catch(e) 
+ {}
+}
