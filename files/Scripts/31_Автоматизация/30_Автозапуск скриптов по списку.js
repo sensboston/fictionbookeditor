a4dc366a1b1b7f2b69d40562c719207b01ae9f8f
User01 <sclex2@gmail.com>
2022-01-13 18:20:29 +0300

    Скрипт "Автозапуск скриптов по списку" обновлен до версии 1.2. В код внесено малозначительное изменение.

--- files/Scripts/31_Автоматизация/30_Автозапуск скриптов по списку.js
+++ files/Scripts/31_Автоматизация/30_Автозапуск скриптов по списку.js
@@ -1,11 +1,12 @@
 // Скрипт "Автозапуск скриптов по списку"
-// Версия 1.1
+// Версия 1.2
 // Автор Sclex
 
 function Run() {
-  window.Run2=function() {
-    apiRunCmd("c:\\\\program files\\\\fbe 2.6.7\\\\scripts\\\\06_Чистка\\\\45_Удалить атрибуты id, на которые нет ссылок, и теги style.js");
-    apiRunCmd("c:\\\\program files\\\\fbe 2.6.7\\\\scripts\\\\06_Чистка\\\\02_Генеральная уборка.js");
-  }
- window.setTimeout("Run2();",0);
+ window.RunSubscripts=function() {
+   apiRunCmd("c:\\\\program files\\\\fbe 2.6.7\\\\scripts\\\\06_Чистка\\\\45_Удалить атрибуты id, на которые нет ссылок, и теги style.js");
+   apiRunCmd("c:\\\\program files\\\\fbe 2.6.7\\\\scripts\\\\06_Чистка\\\\02_Генеральная уборка.js");
+   setTimeout("window.RunSubscripts=undefined;",0);
+ }
+ setTimeout("RunSubscripts()",0);
 }
