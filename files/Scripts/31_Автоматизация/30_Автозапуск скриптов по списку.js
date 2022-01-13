95df8beecf42209e143a58517466e2e3d17080a1
User01 <sclex2@gmail.com>
2022-01-13 17:30:47 +0300

    Создана папка скриптов "31_Автоматизация". В нее положен скрипт "30_Автозапуск скриптов по списку.js" (версия 1.0).

---
+++ files/Scripts/31_Автоматизация/30_Автозапуск скриптов по списку.js
@@ -0,0 +1,13 @@
+// Скрипт "Автозапуск скриптов по списку"
+// Версия 1.0
+// Автор Sclex
+
+function Run() {
+ window.eval(
+  "window.Run2=function() {"+
+  "  apiRunCmd('c:\\\\program files\\\\fbe 2.6.7\\\\scripts\\\\06_Чистка\\\\45_Удалить атрибуты id, на которые нет ссылок, и теги style.js');"+
+  "  apiRunCmd('c:\\\\program files\\\\fbe 2.6.7\\\\scripts\\\\06_Чистка\\\\02_Генеральная уборка.js');"+
+  "}"
+ );
+ window.setTimeout("Run2();",0);
+}
