1521f4b3f115b5f21913f4c9e66a4bca8e50658d
User01 <sclex2@gmail.com>
2022-01-15 11:54:04 +0300

    - В папку "19_Обработка форматирования" добавлены новые скрипты "17_Удалить все аннотации разделов и их содержимое.js" и "18_Расформатировать все аннотации разделов.js". - В той же папке 2 скрипта переименованы в "11_Расформатировать все цитаты.js" и "16_Расформатировать все эпиграфы.js".

--- files/Scripts/19_Обработка форматирования/11_Расформатировать все цитаты.js
+++ files/Scripts/19_Обработка форматирования/11_Расформатировать все цитаты.js
@@ -1,4 +1,4 @@
-// Скрипт "Удалить все цитаты, но оставить их содержимое" v1.0
+// Скрипт "Расформатировать все цитаты" v1.0
 // Автор Sclex
 
 function Run() {
@@ -11,7 +11,7 @@ function Run() {
  for (i=allDivs.length-1; i>=0; i--) {
   if (allDivs[i].className=="cite") {
    if (itsFirstRemoving) {
-    window.external.BeginUndoUnit(document,"удаление всех цитат, но не их содержимого");
+    window.external.BeginUndoUnit(document,"расформатирование всех цитат");
     itsFirstRemoving=false;
    }
    allDivs[i].removeNode(false);
@@ -22,7 +22,7 @@ function Run() {
   MsgBox("Цитат в документе не нашлось.");
   return;
  }
- try { window.external.SetStatusBarText("Удалено цитат: "+removedCitesCnt+"."); }
+ try { window.external.SetStatusBarText("Расформатировано цитат: "+removedCitesCnt+"."); }
  catch(e) {} 
  window.external.EndUndoUnit(document);
 }
