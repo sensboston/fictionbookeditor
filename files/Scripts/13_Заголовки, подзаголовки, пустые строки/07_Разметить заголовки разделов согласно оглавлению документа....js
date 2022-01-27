ee96e6ffbea5676c50a9981cb210b6275225e823
User01 <sclex2@gmail.com>
2022-01-27 13:14:03 +0300

    Добавлен новый скрипт "Разметить заголовки разделов согласно оглавлению документа" v1.7 (в папку "13_Заголовки, подзаголовки, пустые строки").

---
+++ files/Scripts/13_Заголовки, подзаголовки, пустые строки/07_Разметить заголовки разделов согласно оглавлению документа....js
@@ -0,0 +1,39 @@
+//Скрипт «Разметить заголовки разделов согласно оглавлению документа»
+//Автор Sclex
+
+var markupTitlesByContents_versionNum="1.7";
+
+function myBeginUndoUnit(s) {
+ window.external.BeginUndoUnit(document,s);
+}
+ 
+function myEndUndoUnit() {
+  window.external.EndUndoUnit(document);
+}
+
+function Run() {
+
+ var dialogWidth="700px";
+ var dialogHeight="1000px";
+ var fbwBody=document.getElementById("fbw_body");
+ var coll={};
+ coll["fbwBody"]=fbwBody;
+ coll["mainDocument"]=document;
+ coll["window"]=window;
+ coll["versionNum"]=markupTitlesByContents_versionNum;
+ try { var nbspChar=window.external.GetNBSP(); var nbspEntity; if (nbspChar.charCodeAt(0)==160) nbspEntity="&nbsp;"; else nbspEntity=nbspChar; }
+ catch(e) { var nbspChar=String.fromCharCode(160); var nbspEntity="&nbsp;";};
+ coll["nbspChar"]=nbspChar;
+ coll["nbspEntity"]=nbspEntity;
+ if (!document.selection || document.selection.type!="Text") {
+   MsgBox("Вы ничего не выделили.\n\n"+
+          "Перед запуском данного скрипта, пожалуйста, выделите абзацы оглавления.");
+   return;
+ }
+ var myRange=document.selection.createRange();
+ //myRange.expand("sentence");
+ coll["mySelection"]=myRange;
+ var modes=window.showModalDialog("HTML/Разметить заголовки разделов согласно оглавлению документа - основной диалог.htm",coll,
+     "dialogHeight: "+dialogHeight+"; dialogWidth: "+dialogWidth+"; "+
+     "center: Yes; help: No; resizable: Yes; status: No;");
+}
