1797ba91f23a29802f26e5c60ba24b6b669a479a
User01 <sclex2@gmail.com>
2022-01-29 11:30:33 +0300

    Скрипт "Разметить заголовки разделов согласно оглавлению документа" обновлен до версии 2.2. Теперь при разметке заголовков в режиме точного совпадения - из заголовка в документе будут удаляться подстроки типа "[30]", "[30]", "***".

--- files/Scripts/13_Заголовки, подзаголовки, пустые строки/07_Разметить заголовки разделов согласно оглавлению документа....js
+++ files/Scripts/13_Заголовки, подзаголовки, пустые строки/07_Разметить заголовки разделов согласно оглавлению документа....js
@@ -1,7 +1,7 @@
 //Скрипт «Разметить заголовки разделов согласно оглавлению документа»
 //Автор Sclex
 
-var markupTitlesByContents_versionNum="2.1";
+var markupTitlesByContents_versionNum="2.2";
 
 function myBeginUndoUnit(s) {
  window.external.BeginUndoUnit(document,s);
