ce8507a2e99592d6e5d8a7a1e8728b372e07016f
User01 <sclex2@gmail.com>
2022-01-30 12:29:57 +0300

    Скрипт "Разметить заголовки разделов согласно оглавлению документа" обновлен до версии 2.6.

--- files/Scripts/13_Заголовки, подзаголовки, пустые строки/07_Разметить заголовки разделов согласно оглавлению документа....js
+++ files/Scripts/13_Заголовки, подзаголовки, пустые строки/07_Разметить заголовки разделов согласно оглавлению документа....js
@@ -1,7 +1,7 @@
 //Скрипт «Разметить заголовки разделов согласно оглавлению документа»
 //Автор Sclex
 
-var markupTitlesByContents_versionNum="2.5";
+var markupTitlesByContents_versionNum="2.6";
 
 function myBeginUndoUnit(s) {
  window.external.BeginUndoUnit(document,s);
