77082d7f603b8dda3843e927e58351e69bf3ef17
User01 <sclex2@gmail.com>
2022-02-06 17:30:40 +0300

    Скрипт "Разметить заголовки разделов согласно оглавлению документа" обновлен до версии 3.3. В диалог скрипта добавлен (и работает) флажок "Регистр символов размечаемых заголовков делать такой же, как в оглавлении".

--- files/Scripts/13_Заголовки, подзаголовки, пустые строки/07_Разметить заголовки разделов согласно оглавлению документа....js
+++ files/Scripts/13_Заголовки, подзаголовки, пустые строки/07_Разметить заголовки разделов согласно оглавлению документа....js
@@ -1,7 +1,7 @@
 //Скрипт «Разметить заголовки разделов согласно оглавлению документа»
 //Автор Sclex
 
-var markupTitlesByContents_versionNum="3.2";
+var markupTitlesByContents_versionNum="3.3";
 
 function myBeginUndoUnit(s) {
  window.external.BeginUndoUnit(document,s);
