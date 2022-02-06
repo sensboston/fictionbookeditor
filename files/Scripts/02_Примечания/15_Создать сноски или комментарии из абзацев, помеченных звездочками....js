60db5feb9f859adcc51dc57b620129eb426408e0
User01 <sclex2@gmail.com>
2022-02-06 13:40:36 +0300

    Скрипт "Создать сноски или комментарии из абзацев, помеченных звездочками" обновлен до версии 1.6. Изменения: - Раньше скрипт не дорабатывал до конца, если в документе в каком-то из разделов последним подэлементом был блочный элемент (например, цитата). - Раньше иногда не удалялись или удалялись НЕ ВСЕ звездочки в начале абзаца.

--- files/Scripts/02_Примечания/15_Создать сноски или комментарии из абзацев, помеченных звездочками....js
+++ files/Scripts/02_Примечания/15_Создать сноски или комментарии из абзацев, помеченных звездочками....js
@@ -1,7 +1,7 @@
 //Скрипт «Создать сноски или комментарии из абзацев, помеченных звездочками»
 //Автор Sclex
 
-var notesFromSelectedParagraphs_versionNum="1.5";
+var notesFromSelectedParagraphs_versionNum="1.6";
 
 function Run() {
  var dialogWidth="700px";
