bbafe1a145a47ef3571cc81a23293b7551db228a
User01 <sclex2@gmail.com>
2022-02-07 15:57:48 +0300

    Скрипт "Разметить заголовки разделов согласно оглавлению документа" обновлен до версии 3.4. Исправлен баг, из-за которого в размеченных заголовках могли появляться мусорные символы "<" и ">".

--- files/Scripts/13_Заголовки, подзаголовки, пустые строки/07_Разметить заголовки разделов согласно оглавлению документа....js
+++ files/Scripts/13_Заголовки, подзаголовки, пустые строки/07_Разметить заголовки разделов согласно оглавлению документа....js
@@ -1,7 +1,7 @@
 //Скрипт «Разметить заголовки разделов согласно оглавлению документа»
 //Автор Sclex
 
-var markupTitlesByContents_versionNum="3.3";
+var markupTitlesByContents_versionNum="3.4";
 
 function myBeginUndoUnit(s) {
  window.external.BeginUndoUnit(document,s);
