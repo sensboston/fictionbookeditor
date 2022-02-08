3fe2496d13cc5e7d2f8fa21a80fdc283af095628
User01 <sclex2@gmail.com>
2022-02-08 21:02:47 +0300

    Скрипт "Разметить заголовки разделов согласно оглавлению документа" обновлен до версии 3.9. Теперь в многострочном поле ввода в диалоге скрипта абзацы из оглавления выводятся без пустых строк между ними.

--- files/Scripts/13_Заголовки, подзаголовки, пустые строки/07_Разметить заголовки разделов согласно оглавлению документа....js
+++ files/Scripts/13_Заголовки, подзаголовки, пустые строки/07_Разметить заголовки разделов согласно оглавлению документа....js
@@ -1,7 +1,7 @@
 //Скрипт «Разметить заголовки разделов согласно оглавлению документа»
 //Автор Sclex
 
-var markupTitlesByContents_versionNum="3.8";
+var markupTitlesByContents_versionNum="3.9";
 
 function myBeginUndoUnit(s) {
  window.external.BeginUndoUnit(document,s);
