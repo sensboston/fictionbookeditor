8f4ea1db058cfaecafa8b7098c27b9ebda9557c2
User01 <sclex2@gmail.com>
2022-02-23 14:48:46 +0300

    В папку "files/Utilities" добавлена папка "fb2recode" с утилитами для перекодировки fb2-файлов в UTF-8 и Windows-1251.

---
+++ files/Utilities/fb2recode/fb2recodeToUTF-8.js
@@ -0,0 +1,50 @@
+var xmlDoc=new ActiveXObject("Msxml2.DOMDocument.4.0");
+
+function processFile(myPath) {
+  xmlDoc.preserveWhiteSpace = true;
+  xmlDoc.load(myPath);
+  if (xmlDoc.parseError.errorCode) return;
+  var pi = xmlDoc.firstChild;
+  var encoding;
+  if (pi)
+  {
+   attr = pi.attributes;
+   if (attr) {
+    enc = attr.getNamedItem("encoding");
+    if (enc) {
+     if (enc.value==desired_encoding) return;
+     enc.value=desired_encoding;
+    }
+   }
+  }
+  xmlDoc.save(myPath);
+  if (xmlDoc.parseError.errorCode) return;
+  convertedCnt++;
+  WScript.Echo("File "+myPath+" is converted.");
+}
+
+var fso=new ActiveXObject("Scripting.FileSystemObject");
+convertedCnt=0;
+desired_encoding="UTF-8";
+if (WScript.Arguments.Count()==0) {
+ WScript.Echo("Wrong parameters!\n\n"+
+              "Use such format:\n\n"+
+              "cscript fb2recodeToUTF-8.js c:\\file.fb2\n\n"+
+              "or:\n\n"+
+              "cscript fb2recodeToUTF-8.js /dir C:\\books");
+}
+else {
+ if (WScript.Arguments.Item(0)=="/dir") {
+  var files=new Enumerator(fso.GetFolder(WScript.Arguments.Item(1)).files);
+  for (var i=0,file=""; !files.atEnd(); files.moveNext()) {
+   file = files.item() + "";
+   if (fso.GetExtensionName(file).toLowerCase() == "fb2") {
+    processFile(files.item().Path);
+   }
+  }
+ } else {
+  processFile(WScript.Arguments.Item(0));
+ }
+ WScript.Echo("======================================\n"+
+              "How many files are converted: "+convertedCnt+".");
+}
