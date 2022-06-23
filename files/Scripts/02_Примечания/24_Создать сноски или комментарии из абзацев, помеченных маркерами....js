ac186322f6d1d3c42baa44897353c08abac6847e
User01 <sclex2@gmail.com>
2022-06-23 12:48:45 +0300

    Скрипт "24_Создать сноски или комментарии из абзацев, помеченных маркерами..." обновлен до версии 2.1. Теперь можно задавать регэксп или строку для поиска маркеров первого абзаца текстов сносок.

--- files/Scripts/02_Примечания/24_Создать сноски или комментарии из абзацев, помеченных маркерами....js
+++ files/Scripts/02_Примечания/24_Создать сноски или комментарии из абзацев, помеченных маркерами....js
@@ -1,17 +1,20 @@
 //Скрипт «Создать сноски или комментарии из абзацев, помеченных знаками решетки»
 //Автор Sclex
 
-var notesFromSelectedParagraphs_versionNum="2.0";
+var notesFromSelectedParagraphs_versionNum="2.1";
 
 function Run() {
  var dialogWidth="700px";
  var dialogHeight="535px";
  var fbwBody=document.getElementById("fbw_body");
  var coll=new Object();
+ try { var nbspChar=window.external.GetNBSP(); }
+ catch(e) { var nbspChar=String.fromCharCode(160); } 
  coll["fbwBody"]=fbwBody;
  coll["mainDocument"]=document;
  coll["window"]=window;
  coll["versionNum"]=notesFromSelectedParagraphs_versionNum;
+ coll["nbspChar"]=nbspChar;
  var modes=window.showModelessDialog("HTML/Создать сноски или комментарии из абзацев, помеченных маркерами - задание параметров.htm",coll,
      "dialogHeight: "+dialogHeight+"; dialogWidth: "+dialogWidth+"; "+
      "center: Yes; help: No; resizable: Yes; status: No;");
