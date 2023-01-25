db62f1772ea8892252122da837a87ec9ad48e271
User01 <sclex2@gmail.com>
2023-01-25 21:49:47 +0300

    Скрипт "24_Создать сноски или комментарии из абзацев, помеченных маркерами..." обновлен до версии 2.7. Скрипт "15_Создать сноски или комментарии из абзацев, помеченных звездочками..." обновлен до версии 2.3. Скрипт "17_Создать сноски или комментарии из абзацев, помеченных знаками решетки..." обновлен до версии 2.3. Скрипт "19_Создать сноски или комментарии из абзацев, помеченных знаками карет..." обновлен до версии 2.3. - теперь эти скрипты не будут игнорировать первый абзац первого текста сноски, если он (абзац) находится в блочном элементе (например в стихе или в цитате).

--- files/Scripts/02_Примечания/19_Создать сноски или комментарии из абзацев, помеченных знаками карет....js
+++ files/Scripts/02_Примечания/19_Создать сноски или комментарии из абзацев, помеченных знаками карет....js
@@ -1,7 +1,7 @@
 //Скрипт «Создать сноски или комментарии из абзацев, помеченных знаками карет»
 //Автор Sclex
 
-var notesFromSelectedParagraphs_versionNum="2.2";
+var notesFromSelectedParagraphs_versionNum="2.3";
 
 function Run() {
  var dialogWidth="700px";
