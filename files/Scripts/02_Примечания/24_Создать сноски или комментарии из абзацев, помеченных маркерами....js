0cb3ddf6a6b74a19e5de399f3477dabf3e534df1
User01 <sclex2@gmail.com>
2023-01-26 21:00:25 +0300

    Скрипт "24_Создать сноски или комментарии из абзацев, помеченных маркерами..." обновлен до версии 2.8. - теперь не будут игнорироваться абзацы-исходники текста сноски, перед которыми есть абзац, состоящий из звездочек и, возможно, пробелов.

--- files/Scripts/02_Примечания/24_Создать сноски или комментарии из абзацев, помеченных маркерами....js
+++ files/Scripts/02_Примечания/24_Создать сноски или комментарии из абзацев, помеченных маркерами....js
@@ -1,7 +1,7 @@
 //Скрипт «Создать сноски или комментарии из абзацев, помеченных маркерами»
 //Автор Sclex
 
-var notesFromSelectedParagraphs_versionNum="2.7";
+var notesFromSelectedParagraphs_versionNum="2.8";
 
 function Run() {
  var dialogWidth="700px";
