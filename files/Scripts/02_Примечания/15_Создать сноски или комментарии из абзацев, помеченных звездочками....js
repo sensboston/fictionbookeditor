f8b24a1d40ed7d766210165f45534759ab3502ef
User01 <sclex2@gmail.com>
2023-01-27 21:15:07 +0300

    У трех скриптов: - "15_Создать сноски или комментарии из абзацев, помеченных звездочками...", - "17_Создать сноски или комментарии из абзацев, помеченных знаками решетки...", - "19_Создать сноски или комментарии из абзацев, помеченных знаками карет...", - номер версии изменен с 2.4 на 2.5.

--- files/Scripts/02_Примечания/15_Создать сноски или комментарии из абзацев, помеченных звездочками....js
+++ files/Scripts/02_Примечания/15_Создать сноски или комментарии из абзацев, помеченных звездочками....js
@@ -1,7 +1,7 @@
 //Скрипт «Создать сноски или комментарии из абзацев, помеченных звездочками»
 //Автор Sclex
 
-var notesFromSelectedParagraphs_versionNum="2.4";
+var notesFromSelectedParagraphs_versionNum="2.5";
 
 function Run() {
  var dialogWidth="700px";
