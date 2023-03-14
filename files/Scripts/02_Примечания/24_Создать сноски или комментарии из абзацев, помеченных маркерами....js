8aabb830054fe9bb3db598bce170f250c584c760
User01 <sclex2@gmail.com>
2023-03-14 10:02:56 +0300

    Скрипт "24_Создать сноски или комментарии из абзацев, помеченных маркерами..." обновлен до версии 4.6. - раньше могли некорректно обрабатываться пробелы (простые или неразрывные) между маркером первого абзаца текста сноски и последующим текстом того же абзаца. Это происходило, если в качестве маркера первого абзаца текста сноски было выбрано не "задать свой" и не "число надстрочным текстом", а, например "вида [1]".

--- files/Scripts/02_Примечания/24_Создать сноски или комментарии из абзацев, помеченных маркерами....js
+++ files/Scripts/02_Примечания/24_Создать сноски или комментарии из абзацев, помеченных маркерами....js
@@ -1,7 +1,7 @@
 //Скрипт «Создать сноски или комментарии из абзацев, помеченных маркерами»
 //Автор Sclex
 
-var notesFromSelectedParagraphs_versionNum="4.5";
+var notesFromSelectedParagraphs_versionNum="4.6";
 
 function Run() {
  var dialogWidth="700px";
