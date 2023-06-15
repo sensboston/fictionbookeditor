5f1a9a7b0883b613d268150b5cf67f9b76b69120
User01 <sclex2@gmail.com>
2023-06-15 13:24:31 +0300

    Скрипт "24_Создать сноски или комментарии из абзацев, помеченных маркерами..." обновлен до версии 4.7. - теперь по умолчанию в качестве маркера знака сноски и в качестве маркера 1-го абзаца текста сноски будут выбраны опции "Число надстрочным текстом".

--- files/Scripts/02_Примечания/24_Создать сноски или комментарии из абзацев, помеченных маркерами....js
+++ files/Scripts/02_Примечания/24_Создать сноски или комментарии из абзацев, помеченных маркерами....js
@@ -1,7 +1,7 @@
 //Скрипт «Создать сноски или комментарии из абзацев, помеченных маркерами»
 //Автор Sclex
 
-var notesFromSelectedParagraphs_versionNum="4.6";
+var notesFromSelectedParagraphs_versionNum="4.7";
 
 function Run() {
  var dialogWidth="700px";
