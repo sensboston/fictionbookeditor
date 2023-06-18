c599e9d438961852ee356e97966a090ba4346e0c
User01 <sclex2@gmail.com>
2023-06-18 13:25:22 +0300

    Скрипт "07_Разметить заголовки разделов согласно оглавлению документа..." обновлен до версии 4.1 - раньше при включенной галочке "Регистр символов размечаемых заголовков делать такой же, как в оглавлении" изменение регистра заголовка срабатывало только на первом вхождении, т.е. если в документе было два заголовка с одинаковым именем, то изменение регистра срабатывало только на первом.

--- files/Scripts/13_Заголовки, подзаголовки, пустые строки/07_Разметить заголовки разделов согласно оглавлению документа....js
+++ files/Scripts/13_Заголовки, подзаголовки, пустые строки/07_Разметить заголовки разделов согласно оглавлению документа....js
@@ -1,7 +1,7 @@
 //Скрипт «Разметить заголовки разделов согласно оглавлению документа»
 //Автор Sclex
 
-var markupTitlesByContents_versionNum="4.0";
+var markupTitlesByContents_versionNum="4.1";
 
 function myBeginUndoUnit(s) {
  window.external.BeginUndoUnit(document,s);
