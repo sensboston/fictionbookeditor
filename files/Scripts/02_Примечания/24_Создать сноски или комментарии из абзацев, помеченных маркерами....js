a9aed997e9e871f9bc3a6b7cf0c8257484c87ea9
User01 <sclex2@gmail.com>
2023-08-20 15:51:54 +0300

    Скрипт "24_Создать сноски или комментарии из абзацев, помеченных маркерами..." обновлен до версии 5.1. - несколько ускорена работа скрипта в случаях, когда в документе до запуска скрипта имеются цифры надстрочным текстом внутри ссылок. Чем больше в документе таких цифр - тем больше будет ускорение (по сравнению с предыдущей версией скрипта).

--- files/Scripts/02_Примечания/24_Создать сноски или комментарии из абзацев, помеченных маркерами....js
+++ files/Scripts/02_Примечания/24_Создать сноски или комментарии из абзацев, помеченных маркерами....js
@@ -1,7 +1,7 @@
 //Скрипт «Создать сноски или комментарии из абзацев, помеченных маркерами»
 //Автор Sclex
 
-var notesFromSelectedParagraphs_versionNum="5.0";
+var notesFromSelectedParagraphs_versionNum="5.1";
 
 function Run() {
  var dialogWidth="700px";
