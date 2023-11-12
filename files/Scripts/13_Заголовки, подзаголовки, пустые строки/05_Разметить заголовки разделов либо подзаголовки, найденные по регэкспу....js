9df26cbf455578a73eb7f956be52a831735f83fa
User01 <sclex2@gmail.com>
2023-11-12 11:33:26 +0300

    Скрипт "05_Разметить заголовки разделов либо подзаголовки, найденные по регэкспу..." обновлен до версии 3.2. - изменено количество возможных символов, находимых регэкспом "Вида «Заголовок стиха из текста первой строки…»".

--- files/Scripts/13_Заголовки, подзаголовки, пустые строки/05_Разметить заголовки разделов либо подзаголовки, найденные по регэкспу....js
+++ files/Scripts/13_Заголовки, подзаголовки, пустые строки/05_Разметить заголовки разделов либо подзаголовки, найденные по регэкспу....js
@@ -2,7 +2,7 @@
 //Автор Sclex
 //http://www.fictionbook.org/forum/viewtopic.php?t=4412
 
-var markupTitlesOrSubtitles_versionNum="3.1";
+var markupTitlesOrSubtitles_versionNum="3.2";
 
 function Run() {
  var dialogWidth="700px";
