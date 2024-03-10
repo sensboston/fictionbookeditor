ebc44724bb4e4798f8a132aecacd06247eede9ce
User01 <sclex2@gmail.com>
2024-03-10 12:52:37 +0300

    Скрипт "Создать сноски или комментарии из абзацев, помеченных маркерами" обновлен до версии 5.5. - теперь должны корректно работать символы "<", ">" и "&", если они прописаны в полях "Задать свой" для маркера знака сноски либо для маркера 1-го абзаца текста сноски - при этом каждый из флажков "Трактовать как регулярное выражение" может быть как включен, так и выключен.

--- files/Scripts/02_Примечания/24_Создать сноски или комментарии из абзацев, помеченных маркерами....js
+++ files/Scripts/02_Примечания/24_Создать сноски или комментарии из абзацев, помеченных маркерами....js
@@ -1,7 +1,7 @@
 //Скрипт «Создать сноски или комментарии из абзацев, помеченных маркерами»
 //Автор Sclex
 
-var notesFromSelectedParagraphs_versionNum="5.3";
+var notesFromSelectedParagraphs_versionNum="5.5";
 
 function Run() {
  var dialogWidth="700px";
