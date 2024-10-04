e894d4f8e61b82b01802f3ff78a43ff61b5a981c
Sclex <user01@comp1.168.1.104>
2024-10-04 15:43:56 +0300

    Скрипт "30_Запуск скриптов по списку" обновлен до версии 1.5. - в скрипт добавлена возможность делать циклы с использованием скриптов "Перейти на <...>".

--- files/Scripts/31_Автоматизация/30_Запуск скриптов по списку.js
+++ files/Scripts/31_Автоматизация/30_Запуск скриптов по списку.js
@@ -1,5 +1,5 @@
 // Скрипт "Запуск скриптов по списку"
-// Версия 1.4
+// Версия 1.5
 // Автор Sclex
 
 function Run() {
@@ -12,11 +12,16 @@ function Run() {
    
    // Команда, запускающая скрипт из заданной папки скриптов
    // (можно сделать несколько таких строк):
-   runScriptInFolder("06_Чистка\\02_Генеральная уборка.js");
+
+//   while (runScriptInFolder("16_Поиск форматирования\\52_Перейти на следующую цитату.js")=="Found")
+//    runScriptInFolder("\\19_Обработка форматирования\\41_Сформатировать абзац(ы) курсивом.js");
+    
+   while (runScriptInFolder("31_Автоматизация\\06_Поиск (без замены) ошибок текста (версия с исключениями).js")=="Found")
+    runScriptInFolder("19_Обработка форматирования\\41_Сформатировать абзац(ы) курсивом.js");
    
    // Команда, запускающая скрипт по полному пути
    // (можно сделать несколько таких строк):
-   runScript("c:\\program files\\fbe 2.6.7\\scripts\\06_Чистка\\03_Латиница в Кириллице.js");
+   //runScript("c:\\program files\\fbe 2.6.7\\scripts\\06_Чистка\\03_Латиница в Кириллице.js");
    
    setTimeout("window.RunSubscripts=undefined; window.scriptsFolderVar=undefined;",0);
    
@@ -33,11 +38,27 @@ function Run() {
      if (fullPath.substr(fullPath.length-1,1)!="\\" && path.substr(0,1)!="\\")
        fullPath+="\\";
      fullPath+=path;
-     apiRunCmd(fullPath);
+     return myRunCmd(fullPath);
    }
    
    function runScript(path) {
-     apiRunCmd(path);
+     return myRunCmd(path);
+   }
+   
+   function myRunCmd(path) {
+    window.msgText="Скрипт\n\n"+path+"\n\nне был запущен, т.к. не удалось его успешно загрузить.\n\n"+
+                   "Возможная причина – файл скрипта отсутствует по тому пути, где он должен находиться.";
+    window.Run=function() {
+     if (window.msgText) alert(window.msgText);
+    };
+    var script=document.getElementById("userCmd");
+    if(!script)	return;
+    script.src="file:///"+path;
+    var runResult=Run();
+    window.Run=function() {
+     if (window.msgText) alert(window.msgText);
+    }
+    return runResult;
    }
  }
  setTimeout("RunSubscripts()",0);
