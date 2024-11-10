89bc586d21028bfe46c25899b5399fc47fb2704e
Sclex <user01@comp1.168.1.104>
2024-11-10 19:30:30 +0300

    Скрипт "24_Создать сноски или комментарии из абзацев, помеченных маркерами..." обновлен до версии 6.6. - исправлен один баг.

--- files/Scripts/02_Примечания/24_Создать сноски или комментарии из абзацев, помеченных маркерами....js
+++ files/Scripts/02_Примечания/24_Создать сноски или комментарии из абзацев, помеченных маркерами....js
@@ -1,7 +1,7 @@
 //Скрипт «Создать сноски или комментарии из абзацев, помеченных маркерами»
 //Автор Sclex
 
-var notesFromSelectedParagraphs_versionNum="6.5";
+var notesFromSelectedParagraphs_versionNum="6.6";
 
 function Run() {
  var dialogWidth="700px";
