9d4f934d86d9e3bda236c0c3f307ad31782aa1b6
Sclex <sclex2@gmail.com>
2024-12-09 11:51:02 +0300

    Скрипт "15_Создать сноски или комментарии из абзацев, помеченных звездочками..." обновлен до версии 4.3. Скрипт "17_Создать сноски или комментарии из абзацев, помеченных знаками решетки..." обновлен до версии 4.3. Скрипт "19_Создать сноски или комментарии из абзацев, помеченных знаками карет..." обновлен до верси 4.3. - раньше каждый из этих скриптов мог работать некорректно, если в fb2-документе или в поле ввода в диалоге скрипта содержался символ (либо символы) мягкого переноса (десятеричный код этого символа - 173).

--- files/Scripts/02_Примечания/17_Создать сноски или комментарии из абзацев, помеченных знаками решетки....js
+++ files/Scripts/02_Примечания/17_Создать сноски или комментарии из абзацев, помеченных знаками решетки....js
@@ -1,7 +1,7 @@
 //Скрипт «Создать сноски или комментарии из абзацев, помеченных знаками решетки»
 //Автор Sclex
 
-var notesFromSelectedParagraphs_versionNum="4.2";
+var notesFromSelectedParagraphs_versionNum="4.3";
 
 function Run() {
  var dialogWidth="700px";
