f0756e1bb5c479a9875f5af48a251c4075587a7e
Sclex <sclex2@gmail.com>
2024-12-24 21:00:21 +0300

    Обновлены 9 скриптов в папке "16_Поиск форматирования": - 25_Перейти на следующий список.js - 26_Перейти на следующую кириллицу.js - 27_Перейти на следующий абзац с кириллицей.js - 28_Перейти на следующую латиницу.js - 29_Перейти на следующий абзац с латиницей.js - 30_Перейти на следующий КАПС (рус-лат).js - 31_Перейти на следующий абзац с КАПС (рус-лат).js - 32_Перейти на следующую пустую строку.js - 33_Перейти на следующую ссылку.js В каждый из этих скриптов внесено изменение: - теперь скрипты будут корректно работать в случае наличия в fb2-документе символов мягкого переноса (десятичный код этого символа - 173).

--- files/Scripts/16_Поиск форматирования/33_Перейти на следующую ссылку.js
+++ files/Scripts/16_Поиск форматирования/33_Перейти на следующую ссылку.js
@@ -1,5 +1,5 @@
 // Скрипт «Перейти на следующую ссылку» для редактора Fiction Book Editor (FBE).
-// Версия 5.0
+// Версия 5.1
 // Основано на скрипте "Поиск по набору регэкспов"
 // Автор Sclex, сборка TaKir
 // Скрипт производит переход на ближайшую ссылку вниз по тексту.
@@ -376,6 +376,8 @@ function Run() {
  var ltRE_="<";
  var gtRE=new RegExp("&gt;","g");
  var gtRE_=">";
+ var shyRE=new RegExp("&shy;","g");
+ var shyRE_=String.fromCharCode(173);
  var nbspRE=new RegExp("&nbsp;","g");
  var nbspRE_=" ";
  
@@ -411,7 +413,7 @@ function Run() {
    ignoreNullPosition=false; //tr.compareEndPoints("StartToEnd",tr)==0;
 
    el=ptr;
-   s=el.innerHTML.replace(removeTagsRE,removeTagsRE_).replace(imgTagRE,imgTagRE_).replace(ltRE,ltRE_).replace(gtRE,gtRE_).replace(ampRE,ampRE_).replace(nbspRE,nbspRE_);
+   s=el.innerHTML.replace(removeTagsRE,removeTagsRE_).replace(imgTagRE,imgTagRE_).replace(shyRE,shyRE_).replace(ltRE,ltRE_).replace(gtRE,gtRE_).replace(ampRE,ampRE_).replace(nbspRE,nbspRE_);
    s_len=s.length;
    //log+="Входим в searchNext.  s1_len: "+s1_len+"  s_len: "+s_len+"\n\n";
    tr.moveToElementText(el);
@@ -475,8 +477,8 @@ function Run() {
         rslt=regExps[i].exec(s_html);
         flag1=false;
         if (rslt) {
-         newPos=s_html.substr(0,rslt.index).replace(removeTagsRE,removeTagsRE_).replace(imgTagRE,imgTagRE_).replace(ltRE,ltRE_).replace(gtRE,gtRE_).replace(ampRE,ampRE_).replace(nbspRE,nbspRE_).length;
-         rslt_replaced=rslt[0].replace(removeTagsRE,removeTagsRE_).replace(imgTagRE,imgTagRE_).replace(ltRE,ltRE_).replace(gtRE,gtRE_).replace(ampRE,ampRE_).replace(nbspRE,nbspRE_);
+         newPos=s_html.substr(0,rslt.index).replace(removeTagsRE,removeTagsRE_).replace(imgTagRE,imgTagRE_).replace(shyRE,shyRE_).replace(ltRE,ltRE_).replace(gtRE,gtRE_).replace(ampRE,ampRE_).replace(nbspRE,nbspRE_).length;
+         rslt_replaced=rslt[0].replace(removeTagsRE,removeTagsRE_).replace(imgTagRE,imgTagRE_).replace(shyRE,shyRE_).replace(ltRE,ltRE_).replace(gtRE,gtRE_).replace(ampRE,ampRE_).replace(nbspRE,nbspRE_);
          if (ignoreNullPosition ? minPos==s1_html_len+1 : minPos==s1_html_len) break;
          if (rslt_replaced.length==0 || (rslt_replaced.length!=0 && rslt_replaced[0]!="<")) {
           k=regExps[i].lastIndex;
@@ -548,7 +550,7 @@ function Run() {
       if (el && el!=fbwBody) el=el.nextSibling;
      }
     if (el && el.nodeName=="P") {
-     s=el.innerHTML.replace(removeTagsRE,removeTagsRE_).replace(imgTagRE,imgTagRE_).replace(ltRE,ltRE_).replace(gtRE,gtRE_).replace(ampRE,ampRE_).replace(nbspRE,nbspRE_);
+     s=el.innerHTML.replace(removeTagsRE,removeTagsRE_).replace(imgTagRE,imgTagRE_).replace(shyRE,shyRE_).replace(ltRE,ltRE_).replace(gtRE,gtRE_).replace(ampRE,ampRE_).replace(nbspRE,nbspRE_);
      s1_len=0;
      s_html=el.innerHTML;
      s1_html_len=0;
