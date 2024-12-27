903a6eb21796e47ff134de27c940df116cc119a8
Sclex <sclex2@gmail.com>
2024-12-27 12:57:42 +0300

    Скрипт "12_Создать сноски или комментарии из выделенных абзацев..." обновлен до версии 3.1. - теперь состояние флажка "трактовать как регулярное выражение" можно переключать, щелкая мышью по *подписи* к этому флажку.

--- files/Scripts/02_Примечания/12_Создать сноски или комментарии из выделенных абзацев....js
+++ files/Scripts/02_Примечания/12_Создать сноски или комментарии из выделенных абзацев....js
@@ -1,7 +1,7 @@
 //Скрипт «Создать сноски или комментарии из выделенных абзацев»
 //Автор Sclex
 
-var notesFromSelectedParagraphs_versionNum="3.0";
+var notesFromSelectedParagraphs_versionNum="3.1";
 
 function Run() {
  var dialogWidth="700px";
