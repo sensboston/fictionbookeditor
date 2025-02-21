48a6f881ad93ae9d080948a6f31ee24a8a4ade18
Sclex <sclex2@gmail.com>
2025-02-21 21:34:31 +0300

    Скрипт "05_Разметить заголовки разделов либо подзаголовки, найденные по регэкспу..." обновлен до версии 4.0. - в список готовых регэкспов добавлены новые пункты (для документов на английском языке): Вида "Part 123" Вида "Part XVI" Вида "Part One" Вида "Chapter 123" Вида "Chapter XVI" Вида "Chapter One" Вида "Volume 123" Вида "Volume XVI" Вида "Volume One" Вида "Book 123" Вида "Book XVI" Вида "Book One" Вида "Stave 123" Вида "Stave XVI" Вида "Stave One" Prologue, Epilogue, Foreword и пр. - изменен дизайн диалога скрипта. - теперь, если в выпадающем списке с готовыми регэкспами выбрать какой-то вариант, то строка выпадающего списка будет показывать название этого (выбранного) варианта, пока регэксп (в строке ввода) не будет изменен пользователем.

--- files/Scripts/13_Заголовки, подзаголовки, пустые строки/05_Разметить заголовки разделов либо подзаголовки, найденные по регэкспу....js
+++ files/Scripts/13_Заголовки, подзаголовки, пустые строки/05_Разметить заголовки разделов либо подзаголовки, найденные по регэкспу....js
@@ -1,17 +1,31 @@
-//Скрипт «Разметить заголовки разделов или подзаголовки, найденные по регэкспу»
+//Скрипт «Разметить заголовки разделов либо подзаголовки, найденные по регэкспу»
 //Автор Sclex
 //http://www.fictionbook.org/forum/viewtopic.php?t=4412
 
-var markupTitlesOrSubtitles_versionNum="3.3";
+var markupTitlesOrSubtitles_versionNum="4.0";
 
 function Run() {
  var dialogWidth="700px";
- var dialogHeight="295px";
+ var dialogHeight="335px";
+ 
+ // Нижеследующая команда задает список необычных пробелов,
+ // которые в заголовках и подзаголовках должны обрабатываться
+ // наравне с обычными пробелами:
+ var unusualSpaces=String.fromCharCode(8194)+
+  String.fromCharCode(8195)+
+  String.fromCharCode(8196)+
+  String.fromCharCode(8197)+
+  String.fromCharCode(8198)+
+  String.fromCharCode(8239)+
+  String.fromCharCode(8201)+
+  String.fromCharCode(8202);
+  
  var fbwBody=document.getElementById("fbw_body");
  var coll=new Object();
  coll["fbwBody"]=fbwBody;
  coll["mainDocument"]=document;
  coll["window"]=window;
+ coll["unusualSpaces"]=unusualSpaces;
  coll["versionNum"]=markupTitlesOrSubtitles_versionNum;
  var modes=window.showModelessDialog("HTML/Разметить заголовки разделов либо подзаголовки, найденные по регэкспу - задание параметров.htm",coll,
      "dialogHeight: "+dialogHeight+"; dialogWidth: "+dialogWidth+"; "+
