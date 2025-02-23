be066449fe950c1696926da4b0d809300d31c706
Sclex <sclex2@gmail.com>
2025-02-23 16:15:01 +0300

    Новый скрипт "06_Разметить заголовки разделов либо подзаголовки, найденные по регэкспу... (расширенная версия)" (версия 1.7) добавлен в папку скриптов "13_Заголовки, подзаголовки, пустые строки".

---
+++ files/Scripts/13_Заголовки, подзаголовки, пустые строки/06_Разметить заголовки разделов либо подзаголовки, найденные по регэкспу... (расширенная версия).js
@@ -0,0 +1,32 @@
+//Скрипт «Разметить заголовки разделов либо подзаголовки, найденные по регэкспу»
+//Автор Sclex
+
+var markupTitlesOrSubtitles_versionNum="1.7";
+
+function Run() {
+ var dialogWidth="700px";
+ var dialogHeight="400px";
+ 
+ // Нижеследующая команда задает список необычных пробелов,
+ // которые в заголовках и подзаголовках должны обрабатываться
+ // наравне с обычными пробелами:
+ var unusualSpaces=String.fromCharCode(8194)+
+  String.fromCharCode(8195)+
+  String.fromCharCode(8196)+
+  String.fromCharCode(8197)+
+  String.fromCharCode(8198)+
+  String.fromCharCode(8239)+
+  String.fromCharCode(8201)+
+  String.fromCharCode(8202);
+  
+ var fbwBody=document.getElementById("fbw_body");
+ var coll=new Object();
+ coll["fbwBody"]=fbwBody;
+ coll["mainDocument"]=document;
+ coll["window"]=window;
+ coll["unusualSpaces"]=unusualSpaces;
+ coll["versionNum"]=markupTitlesOrSubtitles_versionNum;
+ var modes=window.showModelessDialog("HTML/Разметить заголовки разделов либо подзаголовки, найденные по регэкспу (расширенная версия) - задание параметров.htm",coll,
+     "dialogHeight: "+dialogHeight+"; dialogWidth: "+dialogWidth+"; "+
+     "center: Yes; help: No; resizable: Yes; status: No;");
+}
