d02186374e49bb1eaba4f8da234245803c82320e
Sclex <sclex2@gmail.com>
2025-05-11 13:17:44 +0300

    В папке "16_Поиск форматирования" переименована (TaKir-ом, по моей просьбе) часть скриптов - слова "Перейти на ..." изменены на "Выделить ...". Например, скрипт "35_Перейти на предыдущий целиком курсивный абзац.js" переименован в "35_Выделить предыдущий целиком курсивный абзац.js". Сделано такое переименование потому, что в данной папке сценариев есть как сценарии, которые просто перемещают курсор к началу определенного форматирования, а есть которые именно выделяют определенным образом сформатированный текст.

---
+++ files/Scripts/16_Поиск форматирования/42_Выделить следующий целиком курсивный, но НЕ целиком жирный абзац.js
@@ -0,0 +1,168 @@
+// Скрипт «Выделить следующий целиком курсивный, но НЕ целиком жирный абзац»
+// Автор Sclex
+// Версия 1.2
+
+function Run() {
+
+ var undoMsg="переход на следующий целиком курсивный, но НЕ целиком жирный абзац";
+ var statusBarMsg="Переходим на следующий целиком курсивный, но НЕ целиком жирный абзац…";
+ var notFoundMsg="До конца документа не нашлось ни одного целиком курсивного, но НЕ целиком жирного абзаца.";
+ //имя тэга, который будет использован для маркеров начала и конца выделения
+ var markerTagName="STRIKE";
+ var re0=new RegExp("<STRONG>(((?!</?STRONG>).)*)</STRONG>","ig");
+ var re1=new RegExp("</?[^>]*>","ig");
+ var re5=new RegExp("<EM>(((?!</?EM>).)*)</EM>","ig");
+ var re6=new RegExp("</?[^>]*>","ig");
+ try { var nbspChar=window.external.GetNBSP(); var nbspEntity; if (nbspChar.charCodeAt(0)==160) nbspEntity="&nbsp;"; else nbspEntity=nbspChar; }
+ catch(e) { var nbspChar=String.fromCharCode(160); var nbspEntity="&nbsp;";};
+ var re2=new RegExp(" |&nbsp;|"+nbspChar,"g");
+ 
+ var pos1,myHtml;
+ 
+ function isEntirelyItalic(elem1) {
+  if (isLineEmpty(elem1)) return false;
+  myHtml=elem1.innerHTML;
+  //alert("myHtml на входе: "+myHtml);
+  searchResult=re5.test(myHtml);
+  //alert("searchResult: "+pos1);
+  while (searchResult) {
+   myHtml=myHtml.replace(re5,"");
+   searchResult=re5.test(myHtml);
+  }
+  myHtml=myHtml.replace(re6,"");
+  if (myHtml=="" || myHtml.replace(re2,"")=="") return true;
+  return false;
+ }
+
+ function isEntirelyBold(elem1) {
+  if (isLineEmpty(elem1)) return false;
+  myHtml=elem1.innerHTML;
+  //alert("myHtml на входе: "+myHtml);
+  searchResult=re0.test(myHtml);
+  //alert("searchResult: "+pos1);
+  while (searchResult) {
+   myHtml=myHtml.replace(re0,"");
+   searchResult=re0.test(myHtml);
+  }
+  myHtml=myHtml.replace(re1,"");
+  if (myHtml=="" || myHtml.replace(re2,"")=="") return true;
+  return false;
+ }
+ 
+ var emptyLineRegExp=new RegExp("^( | |&nbsp;|"+nbspChar+")*?$","i");
+ 
+ function isLineEmpty(ptr) {
+  return emptyLineRegExp.test(ptr.innerHTML.replace(/<[^>]*?>/gi,""));
+ }
+    
+ function processPs() {
+ }
+ 
+ function getNextNode(el) {
+  //alert("Вошли в getNextNode.");
+  if (el.firstChild && el.nodeName!="P")
+   el=el.firstChild;
+  else {
+   while (el && !el.nextSibling)
+    el=el.parentNode;
+   if (el && el.nextSibling) el=el.nextSibling; 
+  }
+  return el;
+ }
+ 
+ function getNextP(el) {
+  var savedEl=el;
+  while (el && (el.nodeName!="P" || el==savedEl))
+   el=getNextNode(el);
+  return el;
+ }
+
+ var rndm, startId,endId;
+
+ function insertSelectionMarkers() {
+  rndm=Math.round(Math.random()*100000).toString();
+  startId="BlockStart"+rndm;
+  endId="BlockEnd"+rndm;
+  var tr=document.selection.createRange();
+  var tr2=tr.duplicate();
+  tr.collapse();
+  tr.pasteHTML("<"+markerTagName+" id="+startId+"></"+markerTagName+">");
+  tr2.collapse(false);
+  tr2.pasteHTML("<"+markerTagName+" id="+endId+"></"+markerTagName+">");
+ }
+ 
+ try { var nbspChar=window.external.GetNBSP(); var nbspEntity; if (nbspChar.charCodeAt(0)==160) nbspEntity="&nbsp;"; else nbspEntity=nbspChar;}
+ catch(e) { var nbspChar=String.fromCharCode(160); var nbspEntity="&nbsp;";}
+
+ var s;
+ var ps=[];
+ var tr,el,prv,pm,saveNext,saveFirstEmpty,nextPtr;
+ var errMsg="Нет выделения.\n\nПеред запуском скрипта нужно выделить абзацы, которые будут обработаны.";
+ tr=document.selection.createRange();
+ /* if (!tr || tr.compareEndPoints("StartToEnd",tr)==0) {
+  MsgBox(errMsg);
+  return;
+ } */
+ if (tr.parentElement().nodeName=="TEXTAREA" || tr.parentElement().nodeName=="INPUT") {
+  MsgBox("Ошибка. Должно быть выделение в тексте книги, а не в поле ввода.");
+  return;
+ }
+ window.external.BeginUndoUnit(document,undoMsg);
+ try { window.external.SetStatusBarText(statusBarMsg); }
+ catch(e) {}
+ var fbwBody=document.getElementById("fbw_body");
+
+ var tr3=document.selection.createRange();
+ tr3.collapse(false);
+ var ptr=tr3.parentElement();
+ //alert("blockEndNode: "+ptr.outerHTML);
+ ptr=getNextP(ptr);
+ 
+ /*
+ insertSelectionMarkers();
+ var blockStartNode=document.getElementById(startId);
+ var blockEndNode=document.getElementById(endId);
+ var blockStartEl=blockStartNode;
+ while (blockStartEl && blockStartEl.nodeName!="BODY" && blockStartEl.nodeName!="P")
+  blockStartEl=blockStartEl.parentNode;
+ if (!blockStartEl || blockStartEl.nodeName=="BODY") return;
+ //alert("blockStartEl: "+blockStartEl.outerHTML); 
+ var blockEndEl=blockEndNode;
+ while (blockEndEl && blockEndEl.nodeName!="BODY" && blockEndEl.nodeName!="P")
+  blockEndEl=blockEndEl.parentNode;
+ if (blockEndEl && blockEndEl.nodeName=="BODY") {
+  blockEndEl=blockEndNode;
+  if (blockEndEl.previousSibling && blockEndEl.previousSibling.nodeName=="P")
+   blockEndEl=blockEndEl.previousSibling;
+  if (!blockEndEl || blockEndEl.nodeName!="P") return;
+ }
+ //alert("blockEndEl: "+blockEndEl.outerHTML);
+ var ptr=blockStartEl;*/
+ 
+ while (ptr && fbwBody.contains(ptr)) {
+  if (isEntirelyItalic(ptr) && !isEntirelyBold(ptr)) break;
+  ptr=getNextP(ptr);
+ }
+ 
+ processPs();
+
+ if (ptr) {
+  var tr1=document.body.createTextRange();
+  tr1.moveToElementText(ptr);
+  if (tr1.moveStart("character",1)==1)
+   tr1.moveStart("character",-1);
+  tr1.moveEnd("character",-1);
+  tr1.select();
+  var scriptResult="Found";
+ }
+ else {
+  var scriptResult="NotFound"; 
+  MsgBox(notFoundMsg);
+ }
+
+ //if (processFlag) addPToArray(pp);
+ try { window.external.SetStatusBarText("ОК"); }
+ catch(e) {} 
+ window.external.EndUndoUnit(document);
+ return scriptResult;
+}
---
+++ files/Scripts/16_Поиск форматирования/48_Выделить следующий заголовок.js
@@ -0,0 +1,123 @@
+// Скрипт «Выделить следующий заголовок» (title)
+// Автор Sclex, сборка TaKir
+// Версия 1.3
+
+function Run() {
+
+ var blockElementClass="title"; // класс блочного элемента, который (элемент) нужно искать
+ var undoMsg="переход на следующий заголовок";
+ var statusBarMsg="Переходим на следующий заголовок…";
+ var notFoundMsg="До конца документа не нашлось ни одного заголовка.";
+ 
+ try { var nbspChar=window.external.GetNBSP(); var nbspEntity; if (nbspChar.charCodeAt(0)==160) nbspEntity="&nbsp;"; else nbspEntity=nbspChar; }
+ catch(e) { var nbspChar=String.fromCharCode(160); var nbspEntity="&nbsp;";};
+ var re2=new RegExp(" |&nbsp;|"+nbspChar,"g");
+ 
+ function checkP(elem1) {
+  //if (elem1.className=="text-author") return true;
+  if (hasAmongParents(elem1,"title")) return true;
+  return false;
+ }
+
+ function hasAmongParents(elem2,nameOfClass) {
+  while (elem2 && elem2.nodeName!="BODY") {
+    if (elem2.nodeName=="DIV" && elem2.className==nameOfClass) return true;
+    elem2=elem2.parentNode;
+  }
+  return false;
+ }
+ 
+ function getParentWithClass(elem3,nameOfClass) {
+  while (elem3 && elem3.nodeName!="BODY") {
+    if (elem3.nodeName=="DIV" && elem3.className==nameOfClass) return elem3;
+    elem3=elem3.parentNode;
+  }
+  return null;
+ }
+ 
+ var emptyLineRegExp=new RegExp("^( | |&nbsp;|"+nbspChar+")*?$","i");
+ 
+ function isLineEmpty(ptr) {
+  return emptyLineRegExp.test(ptr.innerHTML.replace(/<[^>]*?>/gi,""));
+ }
+    
+ function getNextNode(el) {
+  //alert("Вошли в getNextNode.");
+  if (el.firstChild && el.nodeName!="P")
+   el=el.firstChild;
+  else {
+   while (el && !el.nextSibling)
+    el=el.parentNode;
+   if (el && el.nextSibling) el=el.nextSibling; 
+  }
+  return el;
+ }
+ 
+ function getNextP(el) {
+  var savedEl=el;
+  while (el && (el.nodeName!="P" || el==savedEl))
+   el=getNextNode(el);
+  return el;
+ }
+ 
+ function scrollIfItNeeds() { 
+  var selection = document.selection;
+  if (selection) {
+    var range = selection.createRange();
+    var rect = range.getBoundingClientRect();
+    // var correction = (rect.bottom - document.documentElement.clientHeight/2); // центр
+   var correction = (rect.bottom - document.documentElement.clientHeight/2); // верх
+   // var popravka = (rect.bottom - document.documentElement.clientHeight/8* 6); // низ
+   window.scrollBy(0, correction);
+  }
+ }
+ 
+ var s;
+ var tr,el,prv,pm,saveNext,saveFirstEmpty,nextPtr;
+ var errMsg="Нет выделения.\n\nПеред запуском скрипта нужно выделить абзацы, которые будут обработаны.";
+ tr=document.selection.createRange();
+
+ if (tr.parentElement().nodeName=="TEXTAREA" || tr.parentElement().nodeName=="INPUT") {
+  MsgBox("Ошибка. Должно быть выделение в тексте книги, а не в поле ввода.");
+  return;
+ }
+ window.external.BeginUndoUnit(document,undoMsg);
+ try { window.external.SetStatusBarText(statusBarMsg); }
+ catch(e) {}
+ var fbwBody=document.getElementById("fbw_body");
+
+ var tr3=document.selection.createRange();
+ tr3.collapse(false);
+ var ptr=tr3.parentElement();
+ var blockElementAtStart=getParentWithClass(ptr,blockElementClass);
+ //alert("blockEndNode: "+ptr.outerHTML);
+ ptr=getNextP(ptr);
+ 
+ while (ptr && fbwBody.contains(ptr)) {
+  if (checkP(ptr)&& getParentWithClass(ptr,blockElementClass)!=blockElementAtStart) break;
+  ptr=getNextP(ptr);
+ }
+ 
+ if (ptr && fbwBody.contains(ptr)) {
+  var tr1=document.body.createTextRange();
+  var parentElem=getParentWithClass(ptr,blockElementClass);
+  if (parentElem) {
+   tr1.moveToElementText(parentElem);
+   if (tr1.moveStart("character",1)==1)
+    tr1.moveStart("character",-1);
+   tr1.moveEnd("character",-1);
+   tr1.select();
+   scrollIfItNeeds();
+   var scriptResult="Found";
+  }
+ }
+ else {
+  var scriptResult="NotFound";
+  MsgBox(notFoundMsg);
+ }
+
+ try { window.external.SetStatusBarText("ОК"); }
+ catch(e) {} 
+ window.external.EndUndoUnit(document);
+ return scriptResult;
+}
