41b1aabb11026a6741e99aa46dfbd359d7376849
Sclex <sclex2@gmail.com>
2025-05-20 09:15:00 +0300

    Скрипт "05_Разметить заголовки разделов либо подзаголовки, найденные по регэкспу..." обновлен до версии 4.3. Скрипт "06_Разметить заголовки разделов либо подзаголовки, найденные по регэкспу... (расширенная версия)" обновлен до версии 2.0. - в каждом из этих скриптов исправлен регэксп для разметки абзацев вида "1.1.1. Имя подраздела".

--- files/Scripts/13_Заголовки, подзаголовки, пустые строки/05_Разметить заголовки разделов либо подзаголовки, найденные по регэкспу....js
+++ files/Scripts/13_Заголовки, подзаголовки, пустые строки/05_Разметить заголовки разделов либо подзаголовки, найденные по регэкспу....js
@@ -1,7 +1,7 @@
 //Скрипт «Разметить заголовки разделов либо подзаголовки, найденные по регэкспу»
 //Автор Sclex
 
-var markupTitlesOrSubtitles_versionNum="4.2";
+var markupTitlesOrSubtitles_versionNum="4.3";
 
 function Run() {
  var dialogWidth="700px";
--- files/Scripts/13_Заголовки, подзаголовки, пустые строки/06_Разметить заголовки разделов либо подзаголовки, найденные по регэкспу... (расширенная версия).js
+++ files/Scripts/13_Заголовки, подзаголовки, пустые строки/06_Разметить заголовки разделов либо подзаголовки, найденные по регэкспу... (расширенная версия).js
@@ -1,7 +1,7 @@
 //Скрипт «Разметить заголовки разделов либо подзаголовки, найденные по регэкспу»
 //Автор Sclex
 
-var markupTitlesOrSubtitles_versionNum="1.9";
+var markupTitlesOrSubtitles_versionNum="2.0";
 
 function Run() {
  var dialogWidth="700px";
