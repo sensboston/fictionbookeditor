8f622b46046039141d93c5d79c12788128462f75
Sclex <sclex2@gmail.com>
2025-09-03 08:42:22 +0300

    Скрипт stokber-а "01_Показать следующую таблицу в HTML-виде" обновлен stokber-ом до версии 1.3. Скрипт stokber-а "02_Показать все-все таблицы в HTML-виде" обновлен stokber-ом до версии 1.3. Информация от stokber-а: "По просьбе добавил в работу обоих сценариев ещё один режим — режим показа таблиц в браузере по умолчанию. Типовой window.open, если быстро и часто его закрывать-открывать, выдавал у TaKir-а временами сообщение об ошибке "Завершение работы системы уже запланировано" (иногда довольно часто). Мне в процессе тестирования пару раз всего удалось воспроизвести такое поведения скрипта. По умолчанию включен штатный режим показа с помощью window.open. Чтобы переключиться в режим браузера по умолчанию, например Chrome, нужно поменять значение переменной display с 0 на 1. Находится она в первых строках кода. Каждый запуск скрипта (по умолчанию) в папке HTML программы FBE будет создаваться временный html-файл temp.html. Также можно назначить любую другую папку для размещения этого файла. По сведениям из сети эта ошибка характерна для window.open в IE11, известна уже более десяти лет, внятного решения не найдено. P.S. Этот, созданный при первом запуске скрипта временный html-файл, каждый следующий запуск будет перезаписываться. Т.е. присутствовать в назначенной директории он будет всегда в количестве одного экземпляра.".

--- files/Scripts/40_Таблицы/01_Показать следующую таблицу в HTML-виде.js
+++ files/Scripts/40_Таблицы/01_Показать следующую таблицу в HTML-виде.js
@@ -2,20 +2,35 @@
 // В скрипте использован код из скрипта 
 // "Перейти на следующую таблицу" (автор Sclex, сборка TaKir).
 // Доработка до заявленного функционала — stokber.
-// Версия 1.2 (август, 2025).
+// Версия 1.3 (сентябрь, 2025).
 
 function Run() {
-var name = "Показать следующую таблицу в HTML-виде";
-var ver = "1.2";
+
+                 ///НАСТРОЙКИ
+    var display = 0; // 0 - показывать в IE, 1 - в браузере по умолчанию.
+
+    // путь для показа в браузере по умолчанию:
+    if (display == 1) {
+       // путь для временного html-файла создаваемого для вашего браузера по умолчанию (HTML/temp.html):
+       var filePatch = document.location.href.replace("file:///", "").replace(/%20/g," ").replace(/main\.html/, "HTML/temp.html");
+       // или указать любой другой путь, например:
+       // var filePatch = "C:\\temp.html"; // Следует иметь ввиду, что скрипт не создаёт новых директорий. Все папки, как и диски указанные в этой переменной, должны реально существовать.
+	}
+    
+    // ================================
+
+    var name = "Показать следующую таблицу в HTML-виде";
+    var ver = "1.3";
+    var pe;
+
     newTable();
     tableHtml();
-    // ---------------------------------------------
+
     function newTable() {
         var blockElementClass = "table"; // класс блочного элемента, который (элемент) нужно искать
         var undoMsg = "переход на следующую таблицу";
         var statusBarMsg = "Переходим на следующую таблицу…";
-        var notFoundMsg = "До конца документа не нашлось ни одной таблицы!\n\nСкрипт '"+name+"' "+ver+".";
-
+        var notFoundMsg = "До конца документа не нашлось ни одной таблицы!\n\nСкрипт '" + name + "' " + ver + ".";
 
         try {
             var nbspChar = window.external.GetNBSP();
@@ -85,8 +100,11 @@ var ver = "1.2";
                 // var correction = (rect.bottom - document.documentElement.clientHeight/2); // верх
                 // var popravka = (rect.bottom - document.documentElement.clientHeight/8* 6); // низ
                 // если таблица занимает более 20 строк сдвигаем её чуть ниже:
-                if (count > 20) {window.scrollBy(0, -100);} else {
-                window.scrollBy(0, 100);} 
+                if (count > 20) {
+                    window.scrollBy(0, -100);
+                } else {
+                    window.scrollBy(0, 100);
+                }
             }
         }
 
@@ -132,11 +150,11 @@ var ver = "1.2";
         } else {
             var scriptResult = "NotFound";
             MsgBox(notFoundMsg);
-    // -------------------------------------------------
+            // -------------------------------------------------
             tr.collapse(false); // Перемещаем курсор в конец выделения.
             tr.select();
             return;
-    // ==========================
+            // ==========================
         }
 
         try {
@@ -154,10 +172,8 @@ var ver = "1.2";
             return false;
         }
 
-        var pe = rng.htmlText; 
+        pe = rng.htmlText;
 
-        // pe = pe.replace(/([^>])\r\n(?=[^<])/gmi, "$1");
-        // pe = pe.replace(/(<(?:P|DIV) id=)([\w]+)/gi, "$1\"$2\"");
         // удаляем все бесполезные для нашей задачи,
         // но глючащие id-ы и fbstyle
         pe = pe.replace(/(<(?:P|DIV)) id=["]?[\w]+["]?( class)/gi, "$1$2");
@@ -170,34 +186,53 @@ var ver = "1.2";
         // убираем префикс fb из атрибутов (только в тегах):
         pe = pe.replace(/fb((?:valign|align|colspan|rowspan)=)(?=[^>]*?>)/ig, "$1"); // !!!
         // Преобразуем:
-       // ячейки:
+        // ячейки:
         pe = pe.replace(/<P class=(td[^>]*?)>([\s\S]+?)<\/P>/ig, "<$1>$2</td>");
         // ячейки-заголовки: 
         pe = pe.replace(/<P class=(th[^>]*?)>([\s\S]+?)<\/P>/ig, "<$1>$2</th>");
         // строки:
         pe = pe.replace(/<DIV class=(tr[^>]*)>([\s\S]+?)<\/DIV>/img, "<$1>$2</tr>");
-       // таблицы: 
+        // таблицы: 
         pe = pe.replace(/<DIV class=(table[^>]*)>([\s\S]+)<\/DIV>/im, "<$1>$2</table>");
 
         //поправка для недовыделенной таблицы состоящей из одной ячейки:
-        if (/^<table[^>]*( [^>]*)?>/m.test(pe) == false && /^<tr( [^>]*)?>/m.test(pe) == false)
-        {
-        pe = pe.replace(/^<td ?([^>]*)?>[^<>]+<\/td>$/im, "<table><tr>$&</tr></table>");
+        if (/^<table[^>]*( [^>]*)?>/m.test(pe) == false && /^<tr( [^>]*)?>/m.test(pe) == false) {
+            // pe = pe.replace(/^<td ?([^>]*)?>[^<>]+<\/td>$/im, "<table><tr>$&</tr></table>");
+			 pe = pe.replace(/<td ?([^>]*)?>.+<\/td>/i, "<table><tr>$&</tr></table>");
+			 pe = pe.replace(/<th ?([^>]*)?>.+<\/th>/i, "<table><tr>$&</tr></table>");
         }
         //поправка для недовыделенной таблицы состоящей из одной строки:
-        if (/^<table( [^>]*)?>/m.test(pe) == false && /^<tr( [^>]*)?>/m.test(pe) == true)
-        {
-         pe = pe.replace(/^[\s\S]+$/im, "<table>$&</table>");
+        if (/^<table( [^>]*)?>/m.test(pe) == false && /^<tr( [^>]*)?>/m.test(pe) == true) {
+            pe = pe.replace(/^[\s\S]+$/im, "<table>$&</table>");
         }
-		
-		// MsgBox(pe);
+
+        // MsgBox(pe);
         // window.clipboardData.setData("text",pe);
 
+    // для показа в IE:
+    if (display == 0) {
         MyMsgWindow(pe);
+    }
+    // для показа в браузере по умолчанию:
+    if (display == 1) {
+        WriteFile();
+    }
 
         function MyMsgWindow(pe) {
             var MsgWindow = window.open("HTML/Показать таблицы в HTML-виде.html", null, "status=no,toolbar=no,menubar=no,location=no,scrollbars=yes,resizable=yes");
             MsgWindow.document.body.innerHTML = pe;
+        }
+
+        function WriteFile() {
+            var shell = new ActiveXObject("WScript.Shell");
+            var pref = '<html><head><meta http-equiv="Content-Type" content="text/html; charset=windows-1251"><meta http-equiv="MSThemeCompatible" content="yes"><title> :: Таблицы в виде HTML :: </title><style type="text/css">body{ color: #000000; font-family: Arial Unicode MS, Candara, Verdana, Arial, Tahoma, Arial, sans-serif; font-size: 10px; text-align:justify;background-color:#FFFFFF; }table {margin-left: 25px;margin-right: auto;margin-top: 25px;margin-bottom: auto;}table, th, td, tr {border: 1px solid black;border-collapse: collapse;padding-left: 15px;padding-right: 15px;padding-bottom: 5px;padding-top: 5px;}</style></head><body>'
+            var suf = "</body></html>";
+            pe = pref + pe + suf;
+            var fso = new ActiveXObject("Scripting.FileSystemObject");
+            var fh = fso.CreateTextFile(filePatch, true);
+            fh.WriteLine(pe);
+            fh.Close();
+            shell.Run("\""+filePatch+"\"");
         }
     }
 }
--- files/Scripts/40_Таблицы/02_Показать все-все таблицы в HTML-виде.js
+++ files/Scripts/40_Таблицы/02_Показать все-все таблицы в HTML-виде.js
@@ -1,9 +1,22 @@
-// Скрипт "Показать все-все таблицы в HTML-виде v.1.2"
-// автор - stokber, (август 2025).
+// Скрипт "Показать все-все таблицы в HTML-виде v.1.3"
+// автор - stokber, (сентябрь, 2025).
 
 function Run() {
+
+                    ///НАСТРОЙКИ
+    var display = 0; // 0 - показывать в IE, 1 - в браузере по умолчанию.
+ 
+    // путь для показа в браузере по умолчанию:
+    if (display == 1) {
+       // путь для временного html-файла создаваемого для вашего браузера по умолчанию (HTML/temp.html):
+       var filePatch = document.location.href.replace("file:///", "").replace(/%20/g," ").replace(/main\.html/, "HTML/temp.html");
+       // или указать любой другой путь, например:
+       // var filePatch = "C:\\temp.html"; // Следует иметь ввиду, что скрипт не создаёт новых директорий. Все папки, как и диски указанные в этой переменной, должны реально существовать.
+	}
+    // ================================
+
     var name = "Показать все-все таблицы в HTML-виде";
-    var ver = "1.2";
+    var ver = "1.3";
     var fbwBody = document.getElementById("fbw_body");
     var str = fbwBody.innerHTML;
 
@@ -32,20 +45,42 @@ function Run() {
     str = str.replace(/<DIV class=table>([\s\S]+?)<\/DIV>/igm, "<table>$1</table>");
     // инфострока Табл.:
     for (var i = 1; i <= countTable; i++) {
-        str = str.replace(new RegExp("<P>Табл.</P>", ""), "<P>Табл. " + i + " из " + countTable + "↓</P>");
+        str = str.replace(new RegExp("<P>Табл.</P>", ""), "<P>Табл. " + i + " из " + countTable + "</P>");
     }
     // интервал между таблицами:
-    str = str.replace(/<\/table>/ig, "$&<BR><BR><BR>");
+    str = str.replace(/<\/table>/ig, "$&<BR><BR><BR><BR>");
 
     if (!str) {
         alert("В документе таблиц не найдено!\n\nСкрипт '" + name + "' v." + ver + ".");
         return;
     }
 
-    MyMsgWindow(str);
-
     function MyMsgWindow(str) {
         var MsgWindow = window.open("HTML/Показать таблицы в HTML-виде.html", null, "status=no,toolbar=no,menubar=no,location=no,scrollbars=yes,resizable=yes");
         MsgWindow.document.body.innerHTML = str;
     }
+
+    // --------------------------------------------------------
+    function WriteFile() {
+
+        var shell = new ActiveXObject("WScript.Shell");
+        var pref = '<html><head><meta http-equiv="Content-Type" content="text/html; charset=windows-1251"><meta http-equiv="MSThemeCompatible" content="yes"><title> :: Таблицы в виде HTML :: </title><style type="text/css">body{ color: #000000; font-family: Arial Unicode MS, Candara, Verdana, Arial, Tahoma, Arial, sans-serif; font-size: 10px; text-align:justify;background-color:#FFFFFF; }table {margin-left: 25px;margin-right: auto;margin-top: 25px;margin-bottom: auto;}table, th, td, tr {border: 1px solid black;border-collapse: collapse;padding-left: 15px;padding-right: 15px;padding-bottom: 5px;padding-top: 5px;}</style></head><body>'
+        var suf = "</body></html>";
+        str = pref + str + suf;
+        var fso = new ActiveXObject("Scripting.FileSystemObject");
+        var fh = fso.CreateTextFile(filePatch, true);
+        fh.WriteLine(str);
+        fh.Close();
+        shell.Run("\""+filePatch+"\"");
+        // ========================================
+    }
+    // для показа в IE:
+    if (display == 0) {
+        MyMsgWindow(str);
+    }
+    // для показа в браузере по умолчанию:
+    if (display == 1) {
+        WriteFile();
+    }
+
 }
