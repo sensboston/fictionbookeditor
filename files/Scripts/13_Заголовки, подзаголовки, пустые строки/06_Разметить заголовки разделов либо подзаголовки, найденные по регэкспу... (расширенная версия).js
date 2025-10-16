a799c8267593a83a1d57b378e9f837c0bf678307
Sclex <sclex2@gmail.com>
2025-10-16 20:56:58 +0300

    Скрипт "06_Разметить заголовки разделов либо подзаголовки, найденные по регэкспу... (расширенная версия)" обновлен до версии 3.0. - раньше, при включенном флажке "абзац должен быть первым непустым абзацем от начала безымянного раздела", абзацы, содержащие только инлайн-картинки, считались за *пустые* абзацы.

--- files/Scripts/13_Заголовки, подзаголовки, пустые строки/06_Разметить заголовки разделов либо подзаголовки, найденные по регэкспу... (расширенная версия).js
+++ files/Scripts/13_Заголовки, подзаголовки, пустые строки/06_Разметить заголовки разделов либо подзаголовки, найденные по регэкспу... (расширенная версия).js
@@ -1,7 +1,7 @@
 //Скрипт «Разметить заголовки разделов либо подзаголовки, найденные по регэкспу»
 //Автор Sclex
 
-var markupTitlesOrSubtitles_versionNum="2.9";
+var markupTitlesOrSubtitles_versionNum="3.0";
 
 function Run() {
  var dialogWidth="700px";
