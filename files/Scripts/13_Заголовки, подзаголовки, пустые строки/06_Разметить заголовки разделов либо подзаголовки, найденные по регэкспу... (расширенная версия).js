0b2fc243dd0b841bfd555bc595697eecc3ef725b
Sclex <sclex2@gmail.com>
2025-10-24 11:45:35 +0300

    Скрипт "06_Разметить заголовки разделов либо подзаголовки, найденные по регэкспу... (расширенная версия)" обновлен до версии 3.1. - доработаны (TaKir-ом) некоторые готовые регэкспы, которые можно выбрать в выпадающем списке в диалоге скрипта.

--- files/Scripts/13_Заголовки, подзаголовки, пустые строки/06_Разметить заголовки разделов либо подзаголовки, найденные по регэкспу... (расширенная версия).js
+++ files/Scripts/13_Заголовки, подзаголовки, пустые строки/06_Разметить заголовки разделов либо подзаголовки, найденные по регэкспу... (расширенная версия).js
@@ -1,7 +1,7 @@
 //Скрипт «Разметить заголовки разделов либо подзаголовки, найденные по регэкспу»
 //Автор Sclex
 
-var markupTitlesOrSubtitles_versionNum="3.0";
+var markupTitlesOrSubtitles_versionNum="3.1";
 
 function Run() {
  var dialogWidth="700px";
