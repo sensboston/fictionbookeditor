76d7afce525e4750875eba9402522949a4ed15b1
Sclex <sclex2@gmail.com>
2025-10-27 10:14:41 +0300

    Скрипт "02_Генеральная уборка" обновлен до версии 2.6. - закомментирована замена по регэкспу re67 (которая ошибочно исправляла текст вида "300 СПАРТАНЦЕВ" на "30 °CПАРТАНЦЕВ").

--- files/Scripts/06_Чистка/02_Генеральная уборка.js
+++ files/Scripts/06_Чистка/02_Генеральная уборка.js
@@ -1,5 +1,5 @@
 //======================================
-//             «Генеральная уборка» (версия 2.5)
+//             «Генеральная уборка» (версия 2.6)
 //                                             Engine by ©Sclex
 //                                                    01.05.2007 – 31.08.2022
 //~~~~~~~~~~~~~~~~~~
@@ -96,7 +96,7 @@
 //v.2.5 — регэкспы (п.п. 142-149) — TaKir
 
 //======================================
-var VersionNumber="2.5";
+var VersionNumber="2.6";
 
 //обрабатывать ли history
 var ObrabotkaHistory=true;
@@ -945,7 +945,7 @@ function Run() {
        if (s.search(re64)!=-1)         { s=s.replace(re64, re64_); }
        if (s.search(re65)!=-1)         { s=s.replace(re65, re65_); }
 //       if (s.search(re66)!=-1)         { s=s.replace(re66, re66_); }                        // и компания Ко
-       if (s.search(re67)!=-1)         { s=s.replace(re67, re67_); }
+//       if (s.search(re67)!=-1)         { s=s.replace(re67, re67_); }
        if (s.search(re68)!=-1)         { s=s.replace(re68, re68_); }
        if (s.search(re69)!=-1)         { s=s.replace(re69, re69_); }
        if (s.search(re70)!=-1)         { s=s.replace(re70, re70_); }
